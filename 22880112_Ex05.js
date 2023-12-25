bea6ddf826c7b18ca21d8199aff22c4df67bd2db
Nhất Trương <truongnhat2492@gmail.com>
2023-12-25 16:01:39 +0700

    Done

--- 22880112_Ex05.js
+++ 22880112_Ex05.js
@@ -1,12 +1,41 @@
-var student = { //object name
+var student = {
   firstName: "Jane", 
   lastName: "Doe",
-  age: 18,
+  age: 38,
   height: 170,
-  fullName: function () { //Object function
+  fullName: function () {
     return this.firstName + " " + this.lastName;
   }
-};
-student.age = 19;
-student[age]++;
-name = student.fullName();
+},
+student2 = {
+  firstName: "Jane", 
+  lastName: "Doe",
+  age: 28,
+  height: 170,
+  fullName: function () {
+    return this.firstName + " " + this.lastName;
+  }
+},
+student3 = {
+  firstName: "Jane", 
+  lastName: "Doe",
+  age: 19,
+  height: 170,
+  fullName: function () {
+    return this.firstName + " " + this.lastName;
+  }
+},
+student4 = {
+  firstName: "Jane", 
+  lastName: "Doe",
+  age: 23,
+  height: 170,
+  fullName: function () {
+    return this.firstName + " " + this.lastName;
+  }
+},
+listStudent = [student, student2, student3, student4];
+listStudent.sort(function (a,b) {
+  return a.age -b.age;
+})
+console.log(listStudent);
